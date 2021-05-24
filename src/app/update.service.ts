f5bed72349e6b0ec6067303afdca78c5e50c8b4f
Felix Brucker <felix@felixbrucker.com>
2021-05-24 11:06:49 +0200

    Auto-reload on update

---
+++ src/app/update.service.ts
@@ -0,0 +1,14 @@
+import {Injectable} from "@angular/core";
+import {SwUpdate} from "@angular/service-worker";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UpdateService {
+  constructor(swUpdate: SwUpdate) {
+    swUpdate.available.subscribe(async () => {
+      await swUpdate.activateUpdate();
+      document.location.reload();
+    });
+  }
+}

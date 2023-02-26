d919a62d306288149c25be955594bab1613c8e58
Felix Brucker <felix@felixbrucker.com>
2023-02-26 22:56:20 +0100

    Update mongoose and simplify export expiry

--- api/export.js
+++ api/export.js
@@ -1,31 +1,25 @@
-const db = require('../lib/services/db');
-const Export = require('../lib/services/db/export');
-const loggingMiddleware = require('../lib/logging-middleware');
-const removeOldExportsMiddleware = require('../lib/remove-old-exports-middleware');
+const db = require('../lib/services/db')
+const Export = require('../lib/services/db/export')
+const loggingMiddleware = require('../lib/logging-middleware')
 
-module.exports = loggingMiddleware(removeOldExportsMiddleware(async (req, res) => {
-  const pools = req.body.pools;
-  const shareKey = req.body.shareKey;
+module.exports = loggingMiddleware(async (req, res) => {
+  const pools = req.body.pools
+  const shareKey = req.body.shareKey
   if (!pools || !shareKey) {
-    return res.status(400).json({
-      error: 'Invalid params!',
-    });
+    return res
+      .status(400)
+      .json({ error: 'Invalid params!' })
   }
-  await db.init();
-  const existingExport = await Export.findOne({
-    shareKey,
-  });
+  await db.init()
+  const existingExport = await Export.findOne({ shareKey })
   if (existingExport) {
-    await Export.deleteOne({
-      shareKey,
-    });
+    await Export.deleteOne({ shareKey })
   }
   const newExport = new Export({
     shareKey,
     pools,
-    createdAt: new Date(),
-  });
-  await newExport.save();
+  })
+  await newExport.save()
 
-  return res.status(201).send();
-}));
+  return res.status(201).send()
+})
--- api/import.js
+++ api/import.js
@@ -1,23 +1,21 @@
-const db = require('../lib/services/db');
-const Export = require('../lib/services/db/export');
-const loggingMiddleware = require('../lib/logging-middleware');
+const db = require('../lib/services/db')
+const Export = require('../lib/services/db/export')
+const loggingMiddleware = require('../lib/logging-middleware')
 
 module.exports = loggingMiddleware(async (req, res) => {
   const shareKey = req.query.shareKey;
   if (!shareKey) {
-    return res.status(400).json({
-      error: 'Invalid params!',
-    });
+    return res
+      .status(400)
+      .json({ error: 'Invalid params!' })
   }
-  await db.init();
-  const existingExport = await Export.findOne({
-    shareKey,
-  });
+  await db.init()
+  const existingExport = await Export.findOne({ shareKey })
   if (!existingExport) {
-    return res.status(404).json({
-      error: 'Unknown export!',
-    });
+    return res
+      .status(404)
+      .json({ error: 'Unknown export!' })
   }
 
   return res.json(existingExport.pools);
-});
+})
